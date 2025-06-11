9bd378110d3fbabe1e09cc4222da4b6676f8f0c5
Rodrigo Cavallari <cast.rodrigos.utic@sebrae.com.br>
2025-06-11 10:01:12 -0300

    entregar desafio
    Atualizado chamada supabase

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,47 +1,34 @@
 import { getGlobalData } from '../../utils/global-data';
-import {
-  getPostBySlug,
-} from '../../utils/mdx-utils';
 
-import { MDXRemote } from 'next-mdx-remote';
-import Head from 'next/head';
-import Link from 'next/link';
-import ArrowIcon from '../../components/ArrowIcon';
-import CustomLink from '../../components/CustomLink';
+import { getPostById } from '../../services/api';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import Layout, { GradientBackground } from '../../components/Layout';
 import SEO from '../../components/SEO';
 
-
-const components = {
-  a: CustomLink,
-  Head,
-};
-
 export default function PostPage({
-  posts,
+  post,
   globalData,
 }) {
   return (
     <Layout>
       <SEO
-        title={`${posts.title} - ${globalData.name}`}
-        description={posts.description}
+        title={`${post.title} - ${globalData.name}`}
+        description={post.description}
       />
       <Header name={globalData.name} />
       <article className="px-6 md:px-0">
         <header>
           <h1 className="text-3xl md:text-5xl dark:text-white text-center mb-12">
-            {posts?.title}
+            {post?.title}
           </h1>
-          {posts?.description && (
-            <p className="text-xl mb-4">{posts?.description}</p>
+          {post?.description && (
+            <p className="text-xl mb-4">{post?.description}</p>
           )}
         </header>
         <main>
           <article className="prose dark:prose-dark">
-            {posts.body}
+            {post.body}
           </article>
         </main>
       </article>
@@ -60,13 +47,12 @@ export default function PostPage({
 
 export const getServerSideProps = async ({ params }) => {
   const globalData = getGlobalData();
-  const posts = await getPostBySlug(params.id);
+  const post = await getPostById(params.id);
  
-
   return {
     props: {
       globalData,
-      posts,
+      post,
     },
   };
 };
--- services/api.js
+++ services/api.js
@@ -1,9 +1,30 @@
-import axios from 'axios';
+import { createClient } from '@supabase/supabase-js'
 
-export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
-    headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_API_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+export const getPosts = async () => {
+
+    let { data: posts, error } = await supabase
+        .from('posts')
+        .select('*');
+
+    return posts;
+
+}
+
+export const getPostById = async (id) => {
+
+    let { data } = await supabase
+        .from('posts')
+        .select('*')
+        .eq('id', id);
+
+    if(!data){
+        return {}
     }
-})
+
+    return data[0];
+    
+}
